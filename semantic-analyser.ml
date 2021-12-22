c57eb2a80655fcc3cb0e3f3a19c924247b99371c
LZL <liorz@rezilion.com>
2021-12-22 18:46:23 +0200

    Fixed typeo in README.md, fixed error in equality function where it would crash for uneven list-lengths and added a to-string method

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -40,6 +40,8 @@ match v1, v2 with
        index1 = index2 && (String.equal name1 name2)
   | _ -> false
 
+let list_eq eq l1 l2 = (List.length l1) = (List.length l2) && List.for_all2 eq l1 l2;;
+
 let rec expr'_eq e1 e2 =
   match e1, e2 with
   | ScmConst' (sexpr1), ScmConst' (sexpr2) -> sexpr_eq sexpr1 sexpr2
@@ -48,23 +50,47 @@ let rec expr'_eq e1 e2 =
                                             (expr'_eq dit1 dit2) &&
                                               (expr'_eq dif1 dif2)
   | (ScmSeq' (exprs1), ScmSeq' (exprs2) | ScmOr' (exprs1), ScmOr' (exprs2)) ->
-        List.for_all2 expr'_eq exprs1 exprs2
+        list_eq expr'_eq exprs1 exprs2
   | (ScmSet' (var1, val1), ScmSet' (var2, val2) | ScmDef' (var1, val1), ScmDef' (var2, val2)) ->
         (var_eq var1 var2) && (expr'_eq val1 val2)
   | ScmLambdaSimple' (vars1, body1), ScmLambdaSimple' (vars2, body2) ->
-     (List.for_all2 String.equal vars1 vars2) && (expr'_eq body1 body2)
+     (list_eq String.equal vars1 vars2) && (expr'_eq body1 body2)
   | ScmLambdaOpt' (vars1, var1, body1), ScmLambdaOpt' (vars2, var2, body2) ->
      (String.equal var1 var2) &&
-       (List.for_all2 String.equal vars1 vars2) && (expr'_eq body1 body2)
+       (list_eq String.equal vars1 vars2) && (expr'_eq body1 body2)
   | ScmApplic' (e1, args1), ScmApplic' (e2, args2) ->
-     (expr'_eq e1 e2) && (List.for_all2 expr'_eq args1 args2)
+     (expr'_eq e1 e2) && (list_eq expr'_eq args1 args2)
   | ScmApplicTP' (e1, args1), ScmApplicTP' (e2, args2) ->
-      (expr'_eq e1 e2) && (List.for_all2 expr'_eq args1 args2)
+      (expr'_eq e1 e2) && (list_eq expr'_eq args1 args2)
   | ScmBox' (v1), ScmBox' (v2) -> var_eq v1 v2
   | ScmBoxGet' (v1), ScmBoxGet' (v2) -> var_eq v1 v2
   | ScmBoxSet' (v1, e1), ScmBoxSet' (v2, e2) -> (var_eq v1 v2) && (expr'_eq e1 e2)
   | _ -> false;;
 
+let unannotate_lexical_address = function
+| (VarFree name | VarParam (name, _) | VarBound (name, _, _)) -> ScmVar name
+
+let rec unanalyze expr' =
+match expr' with
+  | ScmConst' s -> ScmConst(s)
+  | ScmVar' var -> unannotate_lexical_address var
+  | ScmBox' var -> ScmApplic(ScmVar "box", [unannotate_lexical_address var])
+  | ScmBoxGet' var -> unannotate_lexical_address var
+  | ScmBoxSet' (var, expr') -> ScmSet (unannotate_lexical_address var, unanalyze expr')
+  | ScmIf' (test, dit, dif) -> ScmIf (unanalyze test, unanalyze dit, unanalyze dif)
+  | ScmSeq' expr's -> ScmSeq (List.map unanalyze expr's)
+  | ScmSet' (var, expr') -> ScmSet (unannotate_lexical_address var, unanalyze expr')
+  | ScmDef' (var, expr') -> ScmDef (unannotate_lexical_address var, unanalyze expr')
+  | ScmOr' expr's -> ScmOr (List.map unanalyze expr's)
+  | ScmLambdaSimple' (params, expr') ->
+        ScmLambdaSimple (params, unanalyze expr')
+  | ScmLambdaOpt'(params, param, expr') ->
+        ScmLambdaOpt (params, param, unanalyze expr')
+  | (ScmApplic' (expr', expr's) | ScmApplicTP' (expr', expr's)) ->
+        ScmApplic (unanalyze expr', List.map unanalyze expr's);;
+
+let string_of_expr' expr' =
+    string_of_expr (unanalyze expr');;
 
 module type SEMANTIC_ANALYSIS = sig
   val annotate_lexical_addresses : expr -> expr'
@@ -104,20 +130,8 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
 
   (* run this first! *)
   let annotate_lexical_addresses pe = 
-   let rec run pe params env = begin
-      match pe with
-      | ScmConst(sexpr) -> ScmConst'(sexpr)
-      | ScmVar(str) -> ScmVar'((tag_lexical_address_for_var str params env))
-      | ScmIf(test,dit,dif) -> ScmIf'((run test params env),(run dit params env),(run dif params env))
-      | ScmSeq(exprs) -> ScmSeq'(List.map (fun x -> (run x params env)) exprs)
-      | ScmSet(ScmVar(var),expr) -> ScmSet'((tag_lexical_address_for_var var params env),(run expr params env))
-      | ScmDef(ScmVar(var),value) -> ScmDef'(VarFree(var),(run value params env))
-      | ScmOr(exprs) -> ScmOr'(List.map (fun x -> (run x params env)) exprs)
-      | ScmLambdaSimple(args,body) -> ScmLambdaSimple'(args,(run body args (params::env)))
-      | ScmLambdaOpt(args,variable,body) -> ScmLambdaOpt'(args,variable,(run body (args@[variable]) (params::env)))
-      | ScmApplic(expr,exprs) -> ScmApplic'((run expr params env),(List.map (fun x -> run x params env) exprs))
-      | _ -> raise X_this_should_not_happen
-      end
+   let rec run pe params env =
+      raise X_not_yet_implemented 
    in 
    run pe [] [];;
 
@@ -132,33 +146,7 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
   (* run this second! *)
   let annotate_tail_calls pe =
    let rec run pe in_tail =
-      match pe with
-      | ScmConst'(e) -> pe
-      | ScmVar'(e) -> pe
-      | ScmBox'(e) -> pe
-      | ScmBoxGet'(e) -> pe
-      | ScmBoxSet'(e1,e2) -> pe
-      | ScmIf'(test,dit,dif) -> ScmIf'((run test false),(run dit in_tail),(run dif in_tail))
-      | ScmSeq'(lst) -> begin
-                        let first = (List.rev (List.tl (List.rev lst))) in
-                        let last  = (List.hd (List.rev lst)) in
-                        ScmSeq'((List.map (fun x -> (run x false)) first)@[(run last in_tail)] )
-                        end
-      | ScmSet'(var,value) -> ScmSet'(var, (run value false))
-      | ScmDef'(var,value) -> ScmDef'(var, (run value in_tail))
-      | ScmOr'(lst) -> begin
-                        let first = (List.rev (List.tl (List.rev lst))) in
-                        let last  = (List.hd (List.rev lst)) in
-                        ScmOr'((List.map (fun x -> (run x false)) first)@[(run last in_tail)] )
-                        end
-      | ScmLambdaSimple'(args,body) -> ScmLambdaSimple'(args,(run body true))
-      | ScmLambdaOpt'(args,variable,body) -> ScmLambdaOpt'(args,variable,(run body true))
-      | ScmApplic'(expr,exprs) ->begin 
-                                match in_tail with
-                                | true -> ScmApplicTP'((run expr false),(List.map (fun x -> (run x false)) exprs))
-                                | false -> ScmApplic'((run expr false),(List.map (fun x -> (run x false)) exprs))
-                                end
-      | _ -> raise X_this_should_not_happen
+      raise X_not_yet_implemented 
    in 
    run pe false;;
 
